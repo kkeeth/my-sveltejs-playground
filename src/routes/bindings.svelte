5861ee0d976f9cbb846d76974bcc7da60f67fae2
kkeeth <zensin0082@gmail.com>
2021-08-30 00:28:39 +0900

    feat: numeric inputs

--- src/routes/bindings.svelte
+++ src/routes/bindings.svelte
@@ -4,6 +4,8 @@
 
 <script lang="ts">
   let name = 'world';
+  let a = 1;
+  let b = 2;
 </script>
 
 <input bind:value={ name }>
@@ -11,6 +13,17 @@
 
 <hr />
 
+<label>
+  <input type=number bind:value={ a } min=0 max=10>
+  <input type=range bind:value={ a } min=0 max=10>
+</label>
+
+<label>
+  <input type=number bind:value={ b } min=0 max=10>
+  <input type=range bind:value={ b } min=0 max=10>
+</label>
+
+<p>{a} + {b} = {a + b}</p>
 
 <style>
 </style>
