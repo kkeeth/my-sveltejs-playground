6e0c417b18ed95664c49e078f9671b8cc9efebd6
kkeeth <zensin0082@gmail.com>
2021-08-29 19:49:52 +0900

    feat: added events page and dom events

---
+++ src/routes/events.svelte
@@ -0,0 +1,24 @@
+<svelte:head>
+  <title>Events</title>
+</svelte:head>
+
+<script lang="ts">
+  let m = { x: 0, y: 0 };
+
+ const handleMousemove = (event: MouseEvent) => {
+    m.x = event.clientX;
+    m.y = event.clientY;
+  }
+</script>
+
+<div class="mouse-range" on:mousemove={ handleMousemove }>
+  The mouse position is {m.x} x {m.y}
+</div>
+
+<style>
+  .mouse-range {
+    width: 400px;
+    height: 400px;
+    border: 1px solid;
+  }
+</style>
