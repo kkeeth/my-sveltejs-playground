95e894506dfcbc046c3bbffded22c0db68f39213
kkeeth <zensin0082@gmail.com>
2021-08-29 16:48:38 +0900

    feat: added count function

--- src/routes/test.svelte
+++ src/routes/test.svelte
@@ -2,16 +2,41 @@
 	<title>Test</title>
 </svelte:head>
 
-<script>
-  let title = 'This page is my test!!'
+<script lang="ts">
+  let title: string = 'This page is my test!!'
+	let htmlString: string = `this string contains some <strong>HTML!!!</strong>`;
+
+	let count: number = 0;
+	$: doubled = count * 2;
+
+	const incrementCount = () => {
+		count += 1;
+	}
 </script>
 
 <h1>{ title.toUpperCase() }</h1>
+<p>{ @html htmlString }</p>
+
+<hr />
+
+<button on:click={incrementCount}>
+	Clicked { count } { count === 1 ? 'time' : 'times' }
+</button>
+<p>{ count } doubled is { doubled }</p>
 
 <style>
 	h1 {
 		color: purple;
 		font-family: Josefin Sans, 'Comic Sans MS', cursive;
 		font-size: 2rem;
+	}
+
+	button {
+		max-width: 200px;
+		border-radius: 8px;
+	}
+	button:hover {
+		cursor: pointer;
+		background: skyblue;
 	}
 </style>
