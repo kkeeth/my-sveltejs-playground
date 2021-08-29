f3b97d3994b46796c2765ea644ed18c03fa83e0e
kkeeth <zensin0082@gmail.com>
2021-08-30 01:06:39 +0900

    feat: textarea input

--- src/routes/bindings.svelte
+++ src/routes/bindings.svelte
@@ -3,9 +3,13 @@
 </svelte:head>
 
 <script lang="ts">
+  import marked from 'marked';
+
   let name = 'world';
   let a = 1;
   let b = 2;
+  let yes = false;
+  let value = `Some words are *italic*, some are **bold**`;
 </script>
 
 <input bind:value={ name }>
@@ -25,5 +29,31 @@
 
 <p>{a} + {b} = {a + b}</p>
 
+<hr />
+
+<label>
+  <input type=checkbox bind:checked={ yes }>
+  Yes! Send me regular email spam
+</label>
+
+{ #if yes }
+  <p>Thank you. We will bombard your inbox and sell your personal details.</p>
+{ :else }
+  <p>You must opt in to continue. If you're not paying, you're the product.</p>
+{ /if }
+
+<button disabled={ !yes }>
+  Subscribe
+</button>
+
+<hr />
+
+{ @html marked(value) }
+
+<textarea bind:value></textarea>
 <style>
+  textarea {
+    width: 100%;
+    height: 200px;
+  }
 </style>
