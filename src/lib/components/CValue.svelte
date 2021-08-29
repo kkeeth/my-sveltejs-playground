3673f6f82a2baa0423ea342e8335d4cb46e3009a
kkeeth <zensin0082@gmail.com>
2021-08-29 17:18:08 +0900

    feat: splited the count value into separate component

---
+++ src/lib/components/CValue.svelte
@@ -0,0 +1,6 @@
+<script>
+	export let count;
+  $: doubled = count * 2;
+</script>
+
+<p>{ count } doubled is { doubled }</p>
--- src/routes/test.svelte
+++ src/routes/test.svelte
@@ -1,17 +1,23 @@
 <svelte:head>
-	<title>Test</title>
+  <title>Test</title>
 </svelte:head>
 
 <script lang="ts">
+  import CValue from '$lib/components/CValue.svelte';
+
   let title: string = 'This page is my test!!'
-	let htmlString: string = `this string contains some <strong>HTML!!!</strong>`;
+  let htmlString: string = `this string contains some <strong>HTML!!!</strong>`;
 
-	let count: number = 0;
-	$: doubled = count * 2;
+  let count: number = 0;
 
-	const incrementCount = () => {
-		count += 1;
-	}
+  $: if (count >= 10) {
+    console.log(`count is dangerously high!`);
+    count = 9;
+  }
+
+  const incrementCount = () => {
+    count += 1;
+  }
 </script>
 
 <h1>{ title.toUpperCase() }</h1>
@@ -19,24 +25,24 @@
 
 <hr />
 
-<button on:click={incrementCount}>
-	Clicked { count } { count === 1 ? 'time' : 'times' }
+<button on:click={ incrementCount }>
+  Clicked { count } { count === 1 ? 'time' : 'times' }
 </button>
-<p>{ count } doubled is { doubled }</p>
+<CValue count={ count } />
 
 <style>
-	h1 {
-		color: purple;
-		font-family: Josefin Sans, 'Comic Sans MS', cursive;
-		font-size: 2rem;
-	}
+  h1 {
+    color: purple;
+    font-family: Josefin Sans, 'Comic Sans MS', cursive;
+    font-size: 2rem;
+  }
 
-	button {
-		max-width: 200px;
-		border-radius: 8px;
-	}
-	button:hover {
-		cursor: pointer;
-		background: skyblue;
-	}
+  button {
+    max-width: 200px;
+    border-radius: 8px;
+  }
+  button:hover {
+    cursor: pointer;
+    background: skyblue;
+  }
 </style>
